31f9a4ce4488b9a06766c058ae414da366540d46
FomenkoMaksim <maksim.fomenko.1996@gmail.com>
2021-11-09 14:46:55 +0300

    Add - Hello World

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -15,6 +15,7 @@ public class Cinema {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello Java");
+        System.out.println("Hello World");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of rows: ");
         rows = scanner.nextInt();
