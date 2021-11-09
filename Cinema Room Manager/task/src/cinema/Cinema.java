b5b72ff6fe76c6f4ffd9a4e15abdbe358899e431
FomenkoMaksim <maksim.fomenko.1996@gmail.com>
2021-11-09 14:45:35 +0300

    Add - Hello Git

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,4 +1,5 @@
 package cinema;
+import java.net.SocketOption;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -12,6 +13,7 @@ public class Cinema {
     static int totalIncome = 0;
 
     public static void main(String[] args) {
+        System.out.println("Hello Git");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of rows: ");
         rows = scanner.nextInt();
